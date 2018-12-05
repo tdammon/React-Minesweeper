aa712bed058210d789ee9b6a181df3e0a3456604
The Goose <trevordammon@gmail.com>
2018-12-05 17:40:46 -0600

    push to heroku

---
+++ build/precache-manifest.ed7eed37cf1cd7a75be294e2c63e7b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6e7243cea22e68be2cb",
+    "url": "/static/js/main.e6e7243c.chunk.js"
+  },
+  {
+    "revision": "704bc54a5e66292be4db",
+    "url": "/static/js/1.704bc54a.chunk.js"
+  },
+  {
+    "revision": "e6e7243cea22e68be2cb",
+    "url": "/static/css/main.81de33ba.chunk.css"
+  },
+  {
+    "revision": "9de889e689444da85b1fbbce1dff935b",
+    "url": "/index.html"
+  }
+];
